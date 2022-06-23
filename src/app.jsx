f67feb7eed477842a8a3ad07e2f1c35503b73a5b
HAYOON <dimple9347@gmail.com>
2022-06-23 16:56:36 +0900

    make video item

--- src/app.jsx
+++ src/app.jsx
@@ -15,7 +15,7 @@ function App() {
       redirect: 'follow', //fetch를 써서 request를 할 때 옵션을 전달하는 것
     };
     
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&q=vlog&key=AIzaSyBlk3LfwMCisJM00G2Hf1_Ffq94kMx9KlM", requestOptions)
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBlk3LfwMCisJM00G2Hf1_Ffq94kMx9KlM", requestOptions)
       .then(response => response.json()) //text로 적혀있을테지만 json으로 변경해서 가져오는게 좋음.
       .then(result => setVideos(result.items))
       .catch(error => console.log('error', error));
