12c9780eab5b64663d81729d23cd4c646183de22
HAYOON <dimple9347@gmail.com>
2022-06-24 03:21:05 +0900

    Back to the list when searching & result list UI changed

--- src/app.jsx
+++ src/app.jsx
@@ -3,17 +3,27 @@ import styles from './app.module.css';
 import React, { useEffect, useState } from 'react';
 import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/seach_header/search_header';
+import VideoDetail from './components/video_detail/video_detail';
 
 function App({youtube}) {
 
   //처음엔 텅텅 빈 비디오의 목록
   const [videos, setVideos] = useState([]);
   
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  }
+
   const search = query => {
     //query라는 param을 받아오고 이것을 처리해줄 것
+    setSelectedVideo(null);
     youtube
       .search(query)
-      .then(videos => setVideos(videos));
+      .then(videos => {
+        setVideos(videos);
+      });
   }
 
   //마운트가 되어 업데이트가 되면 쓸 수 있음
@@ -28,7 +38,18 @@ function App({youtube}) {
   return (
     <div className={styles.app}>
     <SearchHeader onSearch={search}/>
-    <VideoList videos={videos} />
+    <section className={styles.content}>
+      {selectedVideo && <div className={styles.detail}>
+        <VideoDetail video={selectedVideo} />
+      </div> }
+      <div className={styles.list}>
+        <VideoList 
+          videos={videos} 
+          onVideoClick={selectVideo} 
+          display={ selectedVideo? 'list' : 'grid' }
+        />
+      </div>
+    </section>
     
     </div>
   );
--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -2,22 +2,28 @@ import React from 'react';
 import styles from './video_item.module.css';
 
 //deconstructing 두 번
-const VideoItem = ({video: {snippet}}) => (
-  <li className={styles.container}>
+const VideoItem = ({video, video: {snippet}, onVideoClick, display}) => {
+  
+  const displayType = display === 'list' ? styles.list : styles.grid;
 
-  <div className={styles.video}>
-    <img  className={styles.thumbnail}src={snippet.thumbnails.medium.url}
-    alt="video thumbnail" />
+  return (
+      <li className={`${styles.container} ${displayType}`} onClick={()=>onVideoClick(video)}>
 
-    <div className={styles.metadata}>
-      <p className={styles.title}>
-        {snippet.title}
-      </p>
-      <p className={styles.channel}>
-        {snippet.channelTitle}
-      </p>
-    </div>
-  </div>
-</li>
-)
+      <div className={styles.video}>
+        <img className={styles.thumbnail} src={snippet.thumbnails.medium.url}
+        alt="video thumbnail" />
+
+        <div className={styles.metadata}>
+          <p className={styles.title}>
+            {snippet.title}
+          </p>
+          <p className={styles.channel}>
+            {snippet.channelTitle}
+          </p>
+        </div>
+      </div>
+    </li>
+  );
+};
+
 export default VideoItem;
